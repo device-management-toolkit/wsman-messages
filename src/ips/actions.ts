068838afe62663b4a3656c1190a32eeabf8b8455
Mario Jose Divan Koller <mario.jose.divan.koller@intel.com>
2025-02-28 10:29:32 -0800

    feat(ips): add RequestOSPowerSavingStateChange in PowerManagementService

--- src/ips/actions.ts
+++ src/ips/actions.ts
@@ -11,5 +11,6 @@ export enum Actions {
   START_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/StartOptIn',
   CANCEL_OPT_IN = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/CancelOptIn',
   SEND_OPT_IN_CODE = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_OptInService/SendOptInCode',
-  SET_CERTIFICATES = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_IEEE8021xSettings/SetCertificates'
+  SET_CERTIFICATES = 'http://intel.com/wbem/wscim/1/ips-schema/1/IPS_IEEE8021xSettings/SetCertificates',
+  REQUEST_OS_POWER_SAVING_STATE_CHANG = "http://intel.com/wbem/wscim/1/ips-schema/1/IPS_PowerManagementService/RequestOSPowerSavingStateChange"
 }
