82ea2ec1810cec22dcabaeac9ce0a696a94d65fa
Mario Jose Divan Koller <mario.jose.divan.koller@intel.com>
2025-03-03 09:31:34 -0800

    feat(ips): add RequestOSPowerSavingStateChange in PowerManagementService (#967)

--- src/ips/classes.ts
+++ src/ips/classes.ts
@@ -8,5 +8,6 @@ export enum Classes {
   HOST_BASED_SETUP_SERVICE = 'IPS_HostBasedSetupService',
   ALARM_CLOCK_OCCURRENCE = 'IPS_AlarmClockOccurrence',
   IEEE8021X_SETTINGS = 'IPS_IEEE8021xSettings',
-  IEEE8021X_CREDENTIAL_CONTEXT = 'IPS_8021xCredentialContext'
+  IEEE8021X_CREDENTIAL_CONTEXT = 'IPS_8021xCredentialContext',
+  POWER_MANAGEMENT_SERVICE = 'IPS_PowerManagementService'
 }
