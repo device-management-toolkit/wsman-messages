85bce8c5171d7d8ef6344e24e2a24be098ac285a
sinchubhat <sinchana.s.r@intel.com>
2026-09-18 10:20:58 +0530

    feat(amt): add EnableLocalTimeSync builder for AMT_TimeSynchronizationService
    Add AMT.TimeSynchronizationService.EnableLocalTimeSync(enable) to build the AMT_TimeSynchronizationService/EnableLocalTimeSync WSMAN request
    
    Related to device-management-toolkit/rps#2905

--- src/amt/messages.ts
+++ src/amt/messages.ts
@@ -104,7 +104,8 @@ class AuthorizationService extends Base {
     if (!startIndex) startIndex = 1
     const header: string = this.wsmanMessageCreator.createHeader(Actions.ENUMERATE_USER_ACL_ENTRIES, this.className)
     const body: string = this.wsmanMessageCreator.createBody('EnumerateUserAclEntries_INPUT', this.className, [
-      { StartIndex: startIndex }])
+      { StartIndex: startIndex }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -116,7 +117,8 @@ class AuthorizationService extends Base {
   GetAclEnabledState = (handle: number): string => {
     const header: string = this.wsmanMessageCreator.createHeader(Actions.GET_ACL_ENABLED_STATE, this.className)
     const body: string = this.wsmanMessageCreator.createBody('GetAclEnabledState_INPUT', this.className, [
-      { Handle: handle }])
+      { Handle: handle }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -158,7 +160,8 @@ class AuthorizationService extends Base {
   GetUserAclEntryEx = (handle: number): string => {
     const header: string = this.wsmanMessageCreator.createHeader(Actions.GET_USER_ACL_ENTRY_EX, this.className)
     const body: string = this.wsmanMessageCreator.createBody('GetUserAclEntryEx_INPUT', this.className, [
-      { Handle: handle }])
+      { Handle: handle }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -170,7 +173,8 @@ class AuthorizationService extends Base {
   RemoveUserAclEntry = (handle: number): string => {
     const header: string = this.wsmanMessageCreator.createHeader(Actions.REMOVE_USER_ACL_ENTRY, this.className)
     const body: string = this.wsmanMessageCreator.createBody('RemoveUserAclEntry_INPUT', this.className, [
-      { Handle: handle }])
+      { Handle: handle }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -183,7 +187,8 @@ class AuthorizationService extends Base {
   SetAclEnabledState = (handle: number, enabled: boolean): string => {
     const header: string = this.wsmanMessageCreator.createHeader(Actions.SET_ACL_ENABLED_STATE, this.className)
     const body: string = this.wsmanMessageCreator.createBody('SetAclEnabledState_INPUT', this.className, [
-      { Handle: handle, Enabled: enabled }])
+      { Handle: handle, Enabled: enabled }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -288,7 +293,8 @@ class EthernetPortSettings extends Base {
     const header = this.wsmanMessageCreator.createHeader(Actions.SET_LINK_PREFERENCE, this.className, selector)
     const body = this.wsmanMessageCreator.createBody('SetLinkPreference_INPUT', this.className, [
       { LinkPreference: linkPreference },
-      { Timeout: timeout }])
+      { Timeout: timeout }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 }
@@ -358,7 +364,8 @@ class MessageLog extends Base {
     }
     const header = this.wsmanMessageCreator.createHeader(Actions.GET_RECORDS, this.className)
     const body = this.wsmanMessageCreator.createBody('GetRecords_INPUT', this.className, [
-      { IterationIdentifier: identifier, MaxReadRecords: 390 }])
+      { IterationIdentifier: identifier, MaxReadRecords: 390 }
+    ])
     return this.wsmanMessageCreator.createXml(header, body)
   }
 
@@ -660,6 +667,24 @@ class TimeSynchronizationService extends Base {
     const body: string = this.wsmanMessageCreator.createBody(
       'GetLowAccuracyTimeSynch_INPUT',
       Classes.TIME_SYNCHRONIZATION_SERVICE
+    )
+    return this.wsmanMessageCreator.createXml(header, body)
+  }
+
+  /**
+   * Sets the LocalTimeSyncEnabled property, allowing local software (e.g. LMS) to synchronize AMT's clock.
+   * @param enable Determines if a user with LOCAL_SYSTEM_REALM permission can set the time.
+   * @returns string
+   */
+  EnableLocalTimeSync = (enable: boolean): string => {
+    const header: string = this.wsmanMessageCreator.createHeader(
+      Actions.ENABLE_LOCAL_TIME_SYNC,
+      Classes.TIME_SYNCHRONIZATION_SERVICE
+    )
+    const body: string = this.wsmanMessageCreator.createBody(
+      'EnableLocalTimeSync_INPUT',
+      Classes.TIME_SYNCHRONIZATION_SERVICE,
+      [{ Enable: enable }]
     )
     return this.wsmanMessageCreator.createXml(header, body)
   }
